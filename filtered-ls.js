30e06115fe886de43393fa25152370d9daac2ef6
Tom-ash04 <tomy04@vp.pl>
2017-05-16 17:58:08 +0200

    create module

---
+++ filtered-ls.js
@@ -0,0 +1,12 @@
+var module = require("./module.js")
+var dir = process.argv[2]
+var ext = process.argv[3]
+
+module(dir, ext, function(err, list){
+    if (err) {
+        return console.error("There was an error: ", err)
+    }
+    list.forEach(function (file) {
+        console.log(file)
+    })
+})
